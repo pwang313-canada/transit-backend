f9eff0eab5403545627caa462554cfdf814541eb
pwang313@gmail.com <pwang313@gmail.com>
2026-05-11 23:45:02 -0400

    new solution

--- services/metrolinx.js
+++ services/metrolinx.js
@@ -1,20 +1,18 @@
+require("dotenv").config();
 const axios = require("axios");
 
-const BASE_URL = "https://api.openmetrolinx.com/OpenDataAPI";
-const API_KEY = process.env.METROLINX_API_KEY;
+const BASE_URL = "https://api.openmetrolinx.com/OpenDataAPI/api";
+const API_KEY = process.env.METROLINX_KEY;
+
+if (!API_KEY) {
+  throw new Error("METROLINX_KEY is missing in environment variables");
+}
 
 /**
  * Generic request helper
  */
 async function request(endpoint) {
-    const url = `${BASE_URL}${endpoint}`;
-
-    const res = await fetch(url, {
-        headers: {
-            "x-api-key": API_KEY,
-            "Accept": "application/json"
-        }
-    });
+    const url = `${BASE_URL}${endpoint}?key=${API_KEY}`;
 
     if (!res.ok) {
         const text = await res.text();
@@ -35,7 +33,7 @@ async function request(endpoint) {
  * Get all lines
  */
 async function getScheduleAllLine() {
-    return request("/ScheduleService/Lines");
+    return request("/Schedule/All/Line");
 }
 
 /**
@@ -121,75 +119,78 @@ module.exports = {
 
 
 async function getFares(from, to) {
-    const url = `${BASE_URL}/Fares/${from}/${to}?key=${KEY}`;
+    const url = `${BASE_URL}/Fares/${from}/${to}?key=${API_KEY}`;
     const res = await axios.get(url);
     return res.data;
 }
 
 async function getScheduleTripOnDate(date, from, to, start, maxJourney) {
-    const url = `${BASE_URL}/Schedule/Journey/${date}/${from}/${to}/${start}/${maxJourney}?key=${KEY}`;
+    const url = `${BASE_URL}/Schedule/Journey/${date}/${from}/${to}/${start}/${maxJourney}?key=${API_KEY}`;
     const res = await axios.get(url);
     return res.data;
 }
+
 
 async function getScheduleAllLine(date) {
-    const url = `${BASE_URL}/Schedule/Line/All/${date}?key=${KEY}`;
+    const url = `${BASE_URL}/Schedule/Line/All/${date}?key=${API_KEY}`;
+    console.log("URL:", url);
     const res = await axios.get(url);
     return res.data;
 }
 
+
 async function getScheduleDateLineDirection(date, line, direction) {
-    const url = `${BASE_URL}/Schedule/Line/${date}/${line}/${direction}?key=${KEY}`;
+    const url = `${BASE_URL}/Schedule/Line/${date}/${line}/${direction}?key=${API_KEY}`;
     const res = await axios.get(url);
     return res.data;
 }
 
 async function getScheduleDateTrip(date, trip) {
-    const url = `${BASE_URL}/Schedule/Trip/${date}/${trip}?key=${KEY}`;
+    const url = `${BASE_URL}/Schedule/Trip/${date}/${trip}?key=${API_KEY}`;
     const res = await axios.get(url);
     return res.data;
 }
 
 async function getVehicles() {
-    const url = `${BASE_URL}/Gtfs/FeedVehiclePosition?key=${KEY}`;
+    const url = `${BASE_URL}/Gtfs/FeedVehiclePosition?key=${API_KEY}`;
     const res = await axios.get(url);
     return res.data;
 }
 
 async function getFeedAlerts() {
-    const url = `${BASE_URL}/Gtfs/Feed/Alerts?key=${KEY}`;
+    const url = `${BASE_URL}/Gtfs/Feed/Alerts?key=${API_KEY}`;
     const res = await axios.get(url);
     return res.data;
 }
 
 async function getFeedTripUpdates() {
-    const url = `${BASE_URL}/Gtfs/Feed/TripUpdates?key=${KEY}`;
+    const url = `${BASE_URL}/Gtfs/Feed/TripUpdates?key=${API_KEY}`;
     const res = await axios.get(url);
     return res.data;
 }
 
 async function getServiceAlert() {
-    const url = `${BASE_URL}/ServiceUpdate/ServiceAlert/All?key=${KEY}`;
+    const url = `${BASE_URL}/ServiceUpdate/ServiceAlert/All?key=${API_KEY}`;
     const res = await axios.get(url);
     return res.data;
 }
 async function getInformationAlert() {
-    const url = `${BASE_URL}/ServiceUpdate/InformationAlert/All?key=${KEY}`;
+    const url = `${BASE_URL}/ServiceUpdate/InformationAlert/All?key=${API_KEY}`;
     const res = await axios.get(url);
     return res.data;
 }
 async function getMarketingAlert() {
-    const url = `${BASE_URL}/ServiceUpdate/MarketingAlert/All?key=${KEY}`;
+    const url = `${BASE_URL}/ServiceUpdate/MarketingAlert/All?key=${API_KEY}`;
     const res = await axios.get(url);
     return res.data;
 }
 async function getUnionDeparturesAlert() {
-    const url = `${BASE_URL}/ServiceUpdate/UnionDepartures/All?key=${KEY}`;
+    const url = `${BASE_URL}/ServiceUpdate/UnionDepartures/All?key=${API_KEY}`;
     const res = await axios.get(url);
     return res.data;
 }
 async function getAllException() {
-    const url = `${BASE_URL}/ServiceUpdate/Exceptions/All?key=${KEY}`;
+    const url = `${BASE_URL}/ServiceUpdate/Exceptions/All?key=${API_KEY}`;
     const res = await axios.get(url);
     return res.data;
 }
